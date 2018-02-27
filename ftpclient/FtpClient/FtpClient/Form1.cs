849b7206e0e99f81c27dcc488fed7dd2d17370cb
ok82456 <973349396@qq.com>
2018-02-27 22:36:40 +0800

    first commit

---
+++ ftpclient/FtpClient/FtpClient/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FtpClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = SQLiteHelper.ExecuteDataTable("select * from staff where gh='" + textBox1.Text + "'");
+            if (dt.Rows.Count > 0 && dt.Rows[0]["MIMA"].Equals(textBox2.Text))
+            {
+                Form2 f2 = new Form2();
+                FTPHelper ftp = new FTPHelper("127.0.0.1", "", "public", "123");
+                List<FileStruct> files = ftp.ListFilesAndDirectories();
+                f2.ShowDialog();
+            }
+            MessageBox.Show("用户密码错误");
+        }
+    }
+}
---
+++ ftpclient/FtpClient/FtpClient/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FtpClient
+{
+    public partial class Form2 : Form
+    {
+        public List<FileStruct> dirs;
+        public List<FileStruct> files;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void InitForm()
+        {
+            foreach (FileStruct item in dirs)
+            {
+
+            }
+        }
+    }
+}
